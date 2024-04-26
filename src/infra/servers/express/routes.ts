b10a768ec4106baf0b6ba8da9a5c7af28f528832
MBrayanS <micael.brayan.s@gmail.com>
2024-04-26 17:13:04 -0300

    :sparkles: FEAT: Conecta os controllers ás rotas da aplicação

--- src/infra/servers/express/routes.ts
+++ src/infra/servers/express/routes.ts
@@ -1,5 +1,12 @@
+import 'express-async-errors'
+
 import { Router } from 'express'
 
+import { CadastrarController, LogarController } from '../../../modules/ControllersModule'
+
 const rotas = Router()
+
+rotas.post('/cadastrar', CadastrarController.validarBody, CadastrarController.execute)
+rotas.post('/logar', LogarController.validarBody, LogarController.execute)
 
 export default rotas
