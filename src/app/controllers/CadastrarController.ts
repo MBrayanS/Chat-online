43a649a75fe458d231ab3819046111a87abe5bde
MBrayanS <micael.brayan.s@gmail.com>
2024-04-26 16:45:53 -0300

    :sparkles: FEAT: CadastrarController
    - Controller responsavel por efetuar o cadastro de novos usuarios.

---
+++ src/app/controllers/CadastrarController.ts
@@ -0,0 +1,38 @@
+import { TUsuarioEntityCreateDTO } from '../../core/domain'
+import { ICriarNovoUsuarioUseCase } from '../../core/useCases'
+import { IAutenticacaoService, IValidacaoService } from '../../infra/service'
+import { TEnviarCookiesControllerResponse } from '../adapters'
+
+export interface ICadastrarController {
+    validarBody( params: any, body: TUsuarioEntityCreateDTO ): void
+    execute( params: any, body: TUsuarioEntityCreateDTO ): Promise<TEnviarCookiesControllerResponse>
+}
+
+export class CadastrarController implements ICadastrarController {
+    constructor(
+        protected CriarNovoUsuarioUseCase: ICriarNovoUsuarioUseCase,
+        protected AutenticacaoService: IAutenticacaoService,
+        protected ValidacaoService: IValidacaoService
+    ) {
+        this.validarBody = this.validarBody.bind(this)
+        this.execute = this.execute.bind(this)
+    }
+
+    public validarBody( params: any, body: TUsuarioEntityCreateDTO ) {
+        const regrasDeValidacao = {
+            nome: this.ValidacaoService.Regras.nome,
+            chave: this.ValidacaoService.Regras.chave,
+            senha: this.ValidacaoService.Regras.senha
+        }
+
+        this.ValidacaoService.validarBody(body, regrasDeValidacao)
+    }
+
+    public async execute( params: any, body: TUsuarioEntityCreateDTO ) {
+        const { id, nome } = await this.CriarNovoUsuarioUseCase.execute(body)
+        
+        const token = this.AutenticacaoService.gerarToken({ id, nome })
+
+        return { statusCode: 201, token }
+    }
+}
