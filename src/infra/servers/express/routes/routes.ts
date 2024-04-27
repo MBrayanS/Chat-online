e98132f9e37c9554fdb5e9641a680215111a20a5
MBrayanS <micael.brayan.s@gmail.com>
2024-04-27 17:20:34 -0300

    :sparkles: FEAT: Cria um arquivo para as rotas de views

--- src/infra/servers/express/routes/routes.ts
+++ src/infra/servers/express/routes/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 
+import rotasDasPaginas from './viewsRoutes'
 import rotasDaApi from './apiRoutes'
 
 const rotasExpress = Router()
 
+rotasExpress.use('/', rotasDasPaginas)
 rotasExpress.use('/api', rotasDaApi)
 
 export default rotasExpress
---
+++ src/infra/servers/express/routes/viewsRoutes.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express'
+
+const rotasDasPaginas = Router()
+
+export default rotasDasPaginas
