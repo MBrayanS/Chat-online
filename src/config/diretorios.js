327573d50ab96240d15b1b97a1512b9fcc12e27d
MBrayanS <micael.brayan.s@gmail.com>
2024-04-28 16:53:34 -0300

    :wrench: CHORE Configura as pastas das migrations

--- src/config/diretorios.js
+++ src/config/diretorios.js
@@ -4,4 +4,9 @@ module.exports = {
     public: path.resolve() + '/src/app/public',
     views: path.resolve() + '/src/app/views',
     databaseInMemory: path.resolve() + '/src/infra/databases/db/lapislazuli.db',
+    migrations: {
+        config: path.resolve() + '/src/infra/databases/config.js',
+        models: path.resolve() + '/src/infra/models',
+        path: path.resolve() + '/src/infra/migrations'
+    }
 }
